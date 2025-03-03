c1f17c35a9c08a5d3655a54b0fd8ea0d3a8c0268
Samanthajulian <sjulian4@students.kennesaw.edu>
2025-03-03 18:49:02 -0500

    Create subscriber files
    Edited config to include MySQL, created subscription and testing files

--- config.py
+++ config.py
@@ -2,3 +2,9 @@
 PROJECT_ID = "marine-actor-450915-b0"
 SUBSCRIPTION_ID = "message-subscription"
 TOPIC_ID = "message-topic"
+
+# MySQL Database Credentials
+DB_HOST = "your-mysql-server-host"  # Example: "localhost" or an IP address
+DB_USER = "your-username"
+DB_PASSWORD = "your-password"
+DB_NAME = "your-database-name"
