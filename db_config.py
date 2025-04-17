b0e748916a433a06a6879787197038132e0b4bf3
lreinha1 <lreinha1@students.kennesaw.edu>
2025-04-16 22:40:47 -0400

    Fixing db config and subscriber

--- db_config.py
+++ db_config.py
@@ -2,10 +2,12 @@ import pymysql
 
 def get_db_connection():
     conn = pymysql.connect(
-        host="130.218.6.188",        # replace with your Cloud SQL instance public IP
-        user="test_user1",            # match your config
-        password="is-capstone",       # match your config
+        host="130.218.6.188",
+        user="test_user1",
+        password="is-capstone",
         db="is-capstone",
+        connect_timeout=5,  # add this line
         cursorclass=pymysql.cursors.DictCursor
     )
     return conn
+
