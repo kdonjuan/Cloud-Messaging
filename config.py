0dc2a4ae9e6ab6f0f212b41afb116dbfaf48b5f2
Samanthajulian <sjulian4@students.kennesaw.edu>
2025-04-17 17:19:51 -0400

    db name

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ SUBSCRIPTION_ID = "message-subscription"
 TOPIC_ID = "message-topic"
 
 INSTANCE_CONNECTION_NAME = "marine-actor-450915-b0:us-central1:is-capstone"
-DB_NAME = "is-capstone"
+DB_NAME = "iscapstone-db"
 DB_USER = "test_user1"
 DB_PASSWORD = "is-capstone"
